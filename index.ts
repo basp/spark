fc23bf6fb0895a1e293e3ae11a1e4e8a54d9bd58
Bas Pennings <baspster@gmail.com>
2015-08-20 20:35:58 +0200

    Some extra meta data

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import express = require('express');
 import bodyParser = require('body-parser');
 import levelup = require('levelup');
 import JSONStream = require('JSONStream');
+import merge = require('merge');
 import moment = require('moment');
 
 var db = levelup('./data', {
@@ -18,7 +19,8 @@ app.get('/api/events/:collection', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
 	db.createReadStream({
 		start: req.params.collection + '-',
-		end: req.params.collection + '-~'
+		end: req.params.collection + '-~',
+		limit: 100 // TODO
 	}).
 	pipe(JSONStream.stringify()).
 	pipe(res);
@@ -26,7 +28,13 @@ app.get('/api/events/:collection', (req, res) => {
 
 app.post('/api/events/:collection', (req, res) => {
 	let key = req.params.collection + '-' + moment().format('x');
-	db.put(key, req.body, (err) => {
+	let v = merge.recursive(req.body, {
+		spark: {
+			collection: req.params.collection,
+			timestamp: moment().format('x')
+		}	
+	});
+	db.put(key, v, (err) => {
 		if (err) return res.json({ created: false, error: err });
 		res.json({ created: true });
 	});
